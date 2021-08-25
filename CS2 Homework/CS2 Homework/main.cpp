424f97b69b881f85a8fd99959f593da7292f66f3
kxwan0002 <89234487+kxwan0002@users.noreply.github.com>
2021-08-24 18:10:38 -0700

    First Assignment
    This is a test

---
+++ CS2 Homework/CS2 Homework/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  CS2 Homework
+//
+//  Created by Kam Wan on 8/24/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
+//This is the example of my first code, see if it uploads to Github
